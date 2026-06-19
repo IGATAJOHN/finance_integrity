030f75745b3b62780333cc081e35bd786d4f0b44
IGATAJOHN <igatusjohn15@gmail.com>
2026-06-19 11:25:55 +0100

    Expand candidate seed database and add tracked election period badge to header

--- database.py
+++ database.py
@@ -72,13 +72,50 @@ def seed_initial_data():
             
             # Seed Candidates
             cands = [
+                # Presidential Candidates
                 Candidate(name="Asiwaju Bola Tinubu", category="presidential", party="APC", estimated_spend=135000000000.0, rallies_count=42),
                 Candidate(name="Atiku Abubakar", category="presidential", party="PDP", estimated_spend=112000000000.0, rallies_count=38),
                 Candidate(name="Peter Obi", category="presidential", party="LP", estimated_spend=24000000000.0, rallies_count=29),
+                Candidate(name="Rabiu Kwankwaso", category="presidential", party="NNPP", estimated_spend=18000000000.0, rallies_count=22),
+                Candidate(name="Kola Abiola", category="presidential", party="PRP", estimated_spend=2100000000.0, rallies_count=9),
+                Candidate(name="Prince Adewole Adebayo", category="presidential", party="SDP", estimated_spend=1800000000.0, rallies_count=7),
+
+                # Lagos State Gubernatorial
                 Candidate(name="Babajide Sanwo-Olu", category="gubernatorial", party="APC", state="Lagos", estimated_spend=18500000000.0, rallies_count=15),
-                Candidate(name="Abdulrahman Abdulrazaq", category="gubernatorial", party="APC", state="Kwara", estimated_spend=4200000000.0, rallies_count=8),
+                Candidate(name="Gbadebo Rhodes-Vivour", category="gubernatorial", party="LP", state="Lagos", estimated_spend=5200000000.0, rallies_count=9),
+                Candidate(name="Abdul-Azeez Adediran", category="gubernatorial", party="PDP", state="Lagos", estimated_spend=4800000000.0, rallies_count=8),
+
+                # Oyo State Gubernatorial
                 Candidate(name="Seyi Makinde", category="gubernatorial", party="PDP", state="Oyo", estimated_spend=6800000000.0, rallies_count=12),
+                Candidate(name="Teslim Folarin", category="gubernatorial", party="APC", state="Oyo", estimated_spend=4100000000.0, rallies_count=8),
+
+                # Kwara State Gubernatorial
+                Candidate(name="Abdulrahman Abdulrazaq", category="gubernatorial", party="APC", state="Kwara", estimated_spend=4200000000.0, rallies_count=8),
+                Candidate(name="Yaman Abdullahi", category="gubernatorial", party="PDP", state="Kwara", estimated_spend=1500000000.0, rallies_count=4),
+
+                # Rivers State Gubernatorial
+                Candidate(name="Siminalayi Fubara", category="gubernatorial", party="PDP", state="Rivers", estimated_spend=11200000000.0, rallies_count=14),
+                Candidate(name="Tonye Cole", category="gubernatorial", party="APC", state="Rivers", estimated_spend=6200000000.0, rallies_count=9),
+
+                # Kaduna State Gubernatorial
+                Candidate(name="Uba Sani", category="gubernatorial", party="APC", state="Kaduna", estimated_spend=8800000000.0, rallies_count=11),
+                Candidate(name="Isa Ashiru", category="gubernatorial", party="PDP", state="Kaduna", estimated_spend=5500000000.0, rallies_count=8),
+
+                # Kano State Gubernatorial
+                Candidate(name="Abba Kabir Yusuf", category="gubernatorial", party="NNPP", state="Kano", estimated_spend=9200000000.0, rallies_count=13),
+                Candidate(name="Nasir Yusuf Gawuna", category="gubernatorial", party="APC", state="Kano", estimated_spend=7400000000.0, rallies_count=10),
+
+                # Enugu State Gubernatorial
+                Candidate(name="Peter Mbah", category="gubernatorial", party="PDP", state="Enugu", estimated_spend=5800000000.0, rallies_count=10),
+                Candidate(name="Chijioke Edeoga", category="gubernatorial", party="LP", state="Enugu", estimated_spend=3200000000.0, rallies_count=7),
+
+                # Anambra State Gubernatorial
+                Candidate(name="Charles Soludo", category="gubernatorial", party="APGA", state="Anambra", estimated_spend=6500000000.0, rallies_count=11),
+
+                # Senatorial Candidates
                 Candidate(name="Adams Oshiomhole", category="senatorial", party="APC", state="Edo", estimated_spend=1200000000.0, rallies_count=18),
+                Candidate(name="Godswill Akpabio", category="senatorial", party="APC", state="Akwa Ibom", estimated_spend=1850000000.0, rallies_count=12),
+                Candidate(name="Dave Umahi", category="senatorial", party="APC", state="Ebonyi", estimated_spend=950000000.0, rallies_count=8),
             ]
             db.add_all(cands)
             db.commit()
